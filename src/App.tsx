1c15730f7526a38a0db356c4ad1709c76626cb2a
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-05-20 23:48:46 +0300

    style copyright section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import Footer from "./layout/Footer/Footer";
+import Header from "./layout/Header/Header";
+import HomePage from "./pages/HomePage/HomePage";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div className="border-2 min-h-screen flex flex-col items-center justify-between">
+      <Header />
+      <HomePage />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,5 +1,14 @@
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <div className="border-2 w-full flex flex-col ">
+      <div>bandage</div>
+      <div>info</div>
+      <div className="flex flex-col items-center justify-center bg-[#FAFAFA] text-[#737373] font-bold py-10 sm:text-md leading-6 font-montserrat tracking-wide md:justify-start md:flex-row md:gap-1 md:px-48 *:text-nowrap">
+        <p>Made With Love By</p>
+        <p>Finland All Right Reserved</p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
