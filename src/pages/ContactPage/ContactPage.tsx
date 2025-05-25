5f0955b08c3f4fda92d503e6d9c152566f7c16f1
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-05-25 11:19:18 +0300

    add cta section

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -1,9 +1,11 @@
+import CTA from "./components/CTA/CTA";
 import Hero from "./components/Hero/Hero";
 
 const ContactPage = () => {
   return (
     <div className="w-full max-w-[1400px] flex flex-col items-center justify-start gap-20 mx-auto px-4 py-6 font-montserrat tracking-wide">
       <Hero />
+      <CTA />
     </div>
   );
 };
