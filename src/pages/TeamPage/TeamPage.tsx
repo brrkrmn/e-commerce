ce0b60328452a08616b9460e3e113c9486ace651
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-05-25 23:47:06 +0300

    add hero section

--- src/pages/TeamPage/TeamPage.tsx
+++ src/pages/TeamPage/TeamPage.tsx
@@ -1,7 +1,13 @@
+import Hero from "./components/Hero/Hero";
+import Members from "./components/Members/Members";
+
 const TeamPage = () => {
   return (
-    <div>team</div>
-  )
-}
+    <div className="w-full max-w-[1400px] flex flex-col items-center justify-start font-montserrat tracking-wide py-6 md:py-16 mx-auto">
+      <Hero />
+      <Members />
+    </div>
+  );
+};
 
 export default TeamPage;
