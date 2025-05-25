a68946df55251d3df5ffd461b914c5e9275fb1fb
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-05-25 10:34:18 +0300

    add contact page to router

---
+++ src/pages/ContactPage/ContactPage.tsx
@@ -0,0 +1,7 @@
+const ContactPage = () => {
+  return (
+    <div className="w-full border-2 max-w-[1400px] flex flex-col items-center justify-start gap-20 mx-auto">sd</div>
+  )
+}
+
+export default ContactPage;
