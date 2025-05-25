d2a80008f94b353ab61968d247ba718349de3cf4
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-05-26 00:10:35 +0300

    add free trial section

--- src/pages/TeamPage/TeamPage.tsx
+++ src/pages/TeamPage/TeamPage.tsx
@@ -1,3 +1,4 @@
+import FreeTrial from "./components/FreeTrial/FreeTrial";
 import Hero from "./components/Hero/Hero";
 import Members from "./components/Members/Members";
 
@@ -6,6 +7,7 @@ const TeamPage = () => {
     <div className="w-full max-w-[1400px] flex flex-col items-center justify-start font-montserrat tracking-wide py-6 md:py-16 mx-auto">
       <Hero />
       <Members />
+      <FreeTrial />
     </div>
   );
 };
--- src/pages/TeamPage/components/Members/Members.tsx
+++ src/pages/TeamPage/components/Members/Members.tsx
@@ -3,7 +3,7 @@ import { members } from "./constants";
 
 const Members = () => {
   return (
-    <div className="w-full flex flex-col items-center justify-center text-center gap-12 py-12 md:px-40">
+    <div className="w-full flex flex-col items-center justify-center text-center gap-12 py-20 md:px-40">
       <h2 className="text-4xl text-dark-blue font-bold">Meet Our Team</h2>
       <div className="flex flex-wrap items-center justify-center gap-4">
         {members.map((member, index) => (
@@ -11,7 +11,7 @@ const Members = () => {
         ))}
       </div>
     </div>
-  )
+  );
 }
 
 export default Members;
