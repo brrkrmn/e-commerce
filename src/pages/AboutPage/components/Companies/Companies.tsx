d5a5f4d912012d574f19a6f247ce0d51f61bdd21
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-05-26 11:40:56 +0300

    add companies section

--- src/pages/AboutPage/components/Companies/Companies.tsx
+++ src/pages/AboutPage/components/Companies/Companies.tsx
@@ -1,7 +1,20 @@
+import Clients from "../../../../components/Clients/Clients";
+
 const Companies = () => {
   return (
-    <div>companies</div>
-  )
-}
+    <div className="w-full flex flex-col items-center justify-center text-center gap-2 px-4 bg-light-gray-1 pt-20">
+      <div className="w-full flex flex-col items-center justify-center gap-10">
+        <h2 className="text-dark-blue text-4xl font-bold">
+          Big Companies Are Here
+        </h2>
+        <p className="text-gray font-medium text-sm md:w-[40%]">
+          Problems trying to resolve the conflict between the two major realms
+          of Classical physics: Newtonian mechanics{" "}
+        </p>
+      </div>
+      <Clients />
+    </div>
+  );
+};
 
 export default Companies;
